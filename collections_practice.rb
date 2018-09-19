1b95192eed1258130cd208e57a5fa9fb3bf1406a
Rajvir Rai <rsr1992@gmail.com>
2018-09-19 01:02:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(arr)
+  arr = [25, 1, 7]
+  arr.sort! 
+end
 
+def sort_array_desc(arr)
+  arr = [25, 7, 14]
+  arr.sort.reverse!
+end
+
+def sort_array_char_count(arr)
+  arr = ["dogs", "cat", "Horses"]
+  arr.sort! { |x,y| y <=> x }
+arr
+end
